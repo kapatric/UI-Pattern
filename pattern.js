426cfbb7ae808d3484e2bc136b29776c427f7b1a
kapatric <112514185+kapatric@users.noreply.github.com>
2022-09-28 20:09:42 -0500

    can now access img url from API

--- pattern.js
+++ pattern.js
@@ -1,9 +1,22 @@
 const url = 'https://api.harvardartmuseums.org/image?apikey=9b4d0562-e0ec-4df2-9f9f-6b9399e5e704'
 
+
+let links = document.querySelectorAll("a");
+//
+//links.addEventListener("click", );
 fetch(url)
-  .then(res => res.json())
-  .then(res => console.log("success!", res.records[0]
-  //.then(res => console.log("success!", res.records[0].baseimageurl
-  ))
-
-
+    .then(res => res.json())
+  .then(data => {
+   // console.log(data.records)
+    renderResults(data) 
+  })
+    
+function renderResults(picData) {
+  console.log(picData)
+  picData.records.forEach((pic) => {
+    // console.log(pic.baseimageurl)
+    let a = document.getElementsByTagName("a")
+    let piece = pic.baseimageurl
+    piece.append(a)
+  });
+}
